062bb48c404b10a9be0ea2d2fa58b038d3d1f0d2
WolfySoCute <devilfurfox@gmail.com>
2022-08-27 20:49:15 +0500

    Minor improvements

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// function audioPlay() {
-//     var audio = document.getElementById("audio");
-//     audio.volume = 0.1;
-//     audio.play()
-// }
-
 function mute() {
     var audio = document.getElementById("audio");
     if (audio.paused) {
         document.getElementById("mute-icon").src="src/unmute.svg"
         audio.play();
-        audio.volume = 0.1;
+        audio.volume = 0.2;
     } else if (audio.volume != 0) {
         document.getElementById("mute-icon").src="src/mute.svg"
         audio.volume = 0;
     } else {
         document.getElementById("mute-icon").src="src/unmute.svg"
-        audio.volume = 0.1;
+        audio.volume = 0.2;
     }
 }
