87203d6b7e58cfc38db49d20663664b956496fab
WolfySoCute <devilfurfox@gmail.com>
2022-09-11 02:20:05 +0500

    Bug Fix

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-function mute(elem) {
-    var muteIcon = document.getElementById("mute-icon")
-    if (elem.paused) {
+function mute(audio) {
+    var muteIcon = document.getaudioentById("mute-icon")
+    var audio = document.getaudioentById("audio")
+    if (audio.paused) {
         muteIcon.src="src/unmute.svg";
-        elem.volume = 0.5;
-        elem.currentTime = document.getElementById("video").currentTime;
-        elem.muted = false;
-        elem.play();
-    } else if (elem.muted) {
+        audio.volume = 0.5;
+        audio.currentTime = document.getaudioentById("video").currentTime;
+        audio.muted = false;
+        audio.play();
+    } else if (audio.muted) {
         muteIcon.src="src/unmute.svg";
-        elem.currentTime = document.getElementById("video").currentTime;
-        elem.muted = false;
+        audio.currentTime = document.getaudioentById("video").currentTime;
+        audio.muted = false;
     } else {
         muteIcon.src="src/mute.svg";
-        elem.muted = true;
+        audio.muted = true;
     };
 };
