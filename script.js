1aea441a6235dea6c158638b0ef64815985b7fdd
WolfySoCute <devilfurfox@gmail.com>
2022-09-10 14:28:07 +0500

    Audio currentTime to Video currentTime

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function mute() {
     var audio = document.getElementById("audio");
     if (audio.paused) {
-        document.getElementById("mute-icon").src="src/unmute.svg"
+        document.getElementById("mute-icon").src="src/unmute.svg";
+        audio.volume = 0.5;
+        audio.currentTime = document.getElementById("video").currentTime;
+        audio.muted = false;
         audio.play();
-        audio.volume = 0.2;
-    } else if (audio.volume != 0) {
-        document.getElementById("mute-icon").src="src/mute.svg"
-        audio.volume = 0;
+    } else if (audio.muted) {
+        document.getElementById("mute-icon").src="src/unmute.svg";
+        audio.currentTime = document.getElementById("video").currentTime;
+        audio.muted = false;
     } else {
-        document.getElementById("mute-icon").src="src/unmute.svg"
-        audio.volume = 0.2;
-    }
-}
+        document.getElementById("mute-icon").src="src/mute.svg";
+        audio.muted = true;
+    };
+};
